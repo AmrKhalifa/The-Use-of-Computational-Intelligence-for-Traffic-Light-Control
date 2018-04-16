6ebd0c376387e71a2ebdbb564ac450e4a58df816
Eltayeb Ahmed <eltayeb1000@yahoo.com>
2018-04-16 18:53:07 +0200

    Implemented gradient descent

---
+++ rl_model/gradient_descent/static_controller.py
@@ -0,0 +1,18 @@
+class StaticTrafficLightController:
+    def __init__(self, controller, sequence, timings):
+        self._controller = controller
+        assert (len(sequence) == len(timings))
+        self._n_phases = len(sequence)
+        self._phase_sequence = sequence
+        self._timings = timings
+        self._time_elapsed = 0
+        self._phase_number = 0
+
+    def tick(self):
+        self._time_elapsed += 1
+        if self._time_elapsed == self._timings[self._phase_number]:
+            self._phase_number += 1
+            self._phase_number %= self._n_phases
+            self._time_elapsed = 0
+            self._controller.set_phase(self._phase_sequence[self._phase_number])
+
